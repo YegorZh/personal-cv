b3e97be3941d19955509ea90de3aa60ffbf1d249
Yegor <yegorzhukovskiya@gmail.com>
2022-03-17 18:26:39 +0200

    Quickly configured simple server

---
+++ index.ts
@@ -0,0 +1,8 @@
+const express = require('Express');
+import { Express } from 'express';
+const app = express() as Express;
+
+app.use(express.static('./public'));
+
+const port: string = process.env.PORT || '8000';
+app.listen(port, () => console.log('Listening in port ' + port));
